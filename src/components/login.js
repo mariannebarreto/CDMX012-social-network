57585169d22ee9eea210bd3c9f4b5a66a3a63dcd
mariannebarreto <barretoink@icloud.com>
2022-04-11 21:10:07 -0500

    Modal, responsive (aún no terminado) , faltan likes y unas vistas de escritorio

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable import/no-cycle */
-import { shootIn, googleSignIn, facebookLogin } from '../lib/firebase-auth.js';
+import { shootIn, googleSignIn } from '../lib/firebase-auth.js';
 import { onNavigate } from '../main.js';
 
 export const login = () => {
@@ -36,12 +36,12 @@ export const login = () => {
     googleSignIn();
   });
 
-  const iconF = document.createElement('img');
-  iconF.setAttribute('src', './assets/facebook.png');
-  iconF.addEventListener('click', (e) => {
-    e.preventDefault();
-    facebookLogin();
-  });
+  // const iconF = document.createElement('img');
+  // iconF.setAttribute('src', './assets/facebook.png');
+  // iconF.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   facebookLogin();
+  // });
 
   const infoForm = document.createElement('form');
   infoForm.className = 'formContainer';
@@ -123,7 +123,7 @@ export const login = () => {
   });
 
   header.append(imgArrowBack, titles);
-  containerIcons.append(iconG, iconF);
+  containerIcons.appendChild(iconG);
   infoForm.append(labelMail, inputEmail, labelPassword, inputPassword, maskifyOff, maskifyOn, errorMessage);
   divFooter.append(labelHaveAcc, btnRefJoinUs);
   shootInSection.append(header, logInWith, containerIcons, infoForm, btnInfoShootIn, divFooter);
